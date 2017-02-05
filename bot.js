f55677ced93efba3299305572e8d2f9753f039ae
OmikronLvl <philippe_su@hotmail.fr>
2017-02-05 13:19:25 +0100

    permissions set up

--- bot.js
+++ bot.js
@@ -24,6 +24,23 @@ try{
 	Permissions.users = {};
 }
 
+Permissions.checkPermission = function (user,permission){
+	try {
+		var allowed = true;
+		try{
+			if(Permissions.global.hasOwnProperty(permission)){
+				allowed = Permissions.global[permission] === true;
+			}
+		} catch(e){}
+		try{
+			if(Permissions.users[user.id].hasOwnProperty(permission)){
+				allowed = Permissions.users[user.id][permission] === true;
+			}
+		} catch(e){}
+		return allowed;
+	} catch(e){}
+	return false;
+
 bot.on('ready', () => {
   console.log(bot.user.username+" is ready!");
   load_plugins();
